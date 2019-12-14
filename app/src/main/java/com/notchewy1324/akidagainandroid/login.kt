675b0a4ad11ea8a70f7f575008b0dfbdccc11f7f
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2019-12-14 17:24:29 -0500

    Updated Version
    Got the Login Button to work but crashes when asked to login an account.

--- app/src/main/java/com/notchewy1324/akidagainandroid/login.kt
+++ app/src/main/java/com/notchewy1324/akidagainandroid/login.kt
@@ -3,10 +3,10 @@ package com.notchewy1324.akidagainandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login.*
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.LoginEmail
 import kotlinx.android.synthetic.main.activity_login.LoginPassword
-import java.util.*
 
 class login : AppCompatActivity() {
 
@@ -14,17 +14,26 @@ class login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        //Firebase Vars
         lateinit var auth: FirebaseAuth
         auth = FirebaseAuth.getInstance()
-        auth.signInWithEmailAndPassword(LoginEmail.text.toString(), LoginPassword.text.toString())
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, display a message to the user.
 
-                } else {
-                    // If sign in fails, display a message to the user.
+        Button.setOnClickListener {
+            // [START sign_in_with_email]
+            auth.signInWithEmailAndPassword(LoginEmail.toString(), LoginPassword.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        val user = auth.currentUser
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                    }
                 }
-            }
+            // [END sign_in_with_email]
+
+        }
     }
 }
 
