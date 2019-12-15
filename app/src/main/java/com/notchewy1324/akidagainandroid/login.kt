6ab09a348b8b22e4ba35233b942a42a1b58a3682
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2019-12-15 10:32:14 -0500

    Updated Version

--- app/src/main/java/com/notchewy1324/akidagainandroid/login.kt
+++ app/src/main/java/com/notchewy1324/akidagainandroid/login.kt
@@ -8,11 +8,6 @@ import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.LoginEmail
 import kotlinx.android.synthetic.main.activity_login.LoginPassword
 
-private val Any.isSuccessful: Boolean
-    get() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
 class login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,29 +15,24 @@ class login : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         //Firebase Vars
-        lateinit var auth: FirebaseAuth
-        auth = FirebaseAuth.getInstance()
+        val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
         Button.setOnClickListener {
-            auth.signInWithEmailAndPassword(LoginEmail, LoginPassword)
-                .addOnCompleteListener(this) {
-                    if (it.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Toast.makeText(baseContext, "Authentication failed.",
-                            Toast.LENGTH_SHORT).show()
+            FirebaseAuth.AuthStateListener {
+                auth.signInWithEmailAndPassword(LoginEmail.text.toString(), LoginPassword.text.toString())
+                    .addOnCompleteListener(this) {
+                        when {
+                            it.isSuccessful -> {
+                                // Sign in success, update UI with the signed-in user's information
+                            }
+                            else -> {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(baseContext, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show()
+                            }
+                        }
                     }
-                }
+            }
         }
     }
 }
-
-private fun Any.addOnCompleteListener(login: Any, any: Any) {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-}
-
-private fun Any.signInWithEmailAndPassword(loginEmail: Any, loginPassword: Any) {
-
-}
-
