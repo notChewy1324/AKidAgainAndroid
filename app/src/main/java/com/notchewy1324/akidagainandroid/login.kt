cbbc3d533113318243fe1d685acf4260c3c195b1
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2020-04-25 00:32:07 -0400

    Update login.kt

--- app/src/main/java/com/notchewy1324/akidagainandroid/login.kt
+++ app/src/main/java/com/notchewy1324/akidagainandroid/login.kt
@@ -1,17 +1,38 @@
 package com.notchewy1324.akidagainandroid
 
+import android.R.attr.password
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
+
 class login : AppCompatActivity() {
+
+    private val mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         loginButton.setOnClickListener {
-            //Fix for login code
+            mAuth!!.createUserWithEmailAndPassword(LoginEmail.toString(), LoginPassword.toString())
+                .addOnCompleteListener(
+                    this
+                ) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        LoginDisplayText.text = "Sign up was successful"
+                        setContentView(R.layout.activity_home)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        LoginDisplayText.text = "Sign up has failed"
+                    }
+
+                    // ...
+                }
         }
     }
 }
