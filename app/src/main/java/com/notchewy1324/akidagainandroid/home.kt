c11484f940d71ecdecd5a4554d24e9bc68ad2f18
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2020-04-25 00:18:10 -0400

    Update home.kt

--- app/src/main/java/com/notchewy1324/akidagainandroid/home.kt
+++ app/src/main/java/com/notchewy1324/akidagainandroid/home.kt
@@ -2,6 +2,7 @@ package com.notchewy1324.akidagainandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class home : AppCompatActivity() {
 
@@ -9,6 +10,21 @@ class home : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        //Button Links
+        momentsButton.setOnClickListener {
+            //Fix Link
+        }
 
+        chaptersButton.setOnClickListener {
+            //Fix Link
+        }
+
+        communityButton.setOnClickListener {
+            //Fix Link
+        }
+
+        historyButton.setOnClickListener {
+            //Fix Link
+        }
     }
 }
