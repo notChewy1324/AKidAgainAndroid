4e2146159d78c9bb473e5d2b3ac4c75c3d7d9adf
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2020-04-25 00:12:26 -0400

    Update signup.kt

--- app/src/main/java/com/notchewy1324/akidagainandroid/signup.kt
+++ app/src/main/java/com/notchewy1324/akidagainandroid/signup.kt
@@ -2,11 +2,16 @@ package com.notchewy1324.akidagainandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class signup : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        signUp_Button.setOnClickListener {
+            //Fix for sign up code
+        }
     }
 }
