612912d4ac2b16bbfa0790d2c243c2aba49ab5b3
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2020-04-25 00:12:30 -0400

    Update login.kt

--- app/src/main/java/com/notchewy1324/akidagainandroid/login.kt
+++ app/src/main/java/com/notchewy1324/akidagainandroid/login.kt
@@ -1,18 +1,8 @@
 package com.notchewy1324.akidagainandroid
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.activity_login.Button
-import android.widget.EditText
-
-
-private val FirebaseAuth.isSuccessful: Any
-    get() {
-        return Any()
-    }
 
 class login : AppCompatActivity() {
 
@@ -20,35 +10,8 @@ class login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        //Firebase Vars
-        var auth = FirebaseAuth.getInstance()
-
-        Button.setOnClickListener {
-            FirebaseAuth.AuthStateListener {
-                auth.signInWithEmailAndPassword(LoginEmail, LoginPassword)
-                    .addOnCompleteListener(this) {
-                        when {
-                            it.isSuccessful as Boolean -> {
-                                // Sign in success, update UI with the signed-in user's information
-                                Toast.makeText(baseContext, "Authentication Successful.",
-                                    Toast.LENGTH_SHORT).show()
-                            }
-                            else -> {
-                                // If sign in fails, display a message to the user.
-                                Toast.makeText(baseContext, "Authentication Failed.",
-                                    Toast.LENGTH_SHORT).show()
-                            }
-                        }
-                    }
-            }
+        loginButton.setOnClickListener {
+            //Fix for login code
         }
     }
-}
-
-private fun Unit.addOnCompleteListener(login: login, function: () -> Unit) {
-
-}
-
-private fun FirebaseAuth.signInWithEmailAndPassword(LoginEmail: EditText?, LoginPassword: EditText?) {
-
 }
